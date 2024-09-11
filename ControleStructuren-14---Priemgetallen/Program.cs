7c3de4a0a10fca37e7f3dbeb3e0af7be07ec4e3e
Koen Bloemen <koen.bloemen@pxl.be>
2024-09-11 10:16:28 +0200

    final

---
+++ ControleStructuren-14---Priemgetallen/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+for (int row = 0; row < 10; row++)
+{
+    for (int col = 1; col <= 10; col++)
+    {
+        int number = (row * 10) + col;
+
+        if (IsPrimeNumber(number))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+
+        Console.Write("\t" + number);
+    }
+
+    Console.WriteLine();
+}
+
+Console.ReadKey();
+
+static bool IsPrimeNumber(int number)
+{
+    if (number <= 1)
+        return false;
+
+    for (int i = 2; i <= (number / 2); i++)
+    {
+        if (number % i == 0)
+            return false;
+    }
+
+    return true;
+}
